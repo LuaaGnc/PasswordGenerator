45b9078131728a143c6bd252a441a3816d8f9378
Luis-FutEng <luisgvs0302@gmail.com>
2021-02-09 00:04:49 +0000

    Update - Fixing Bugs

--- db/escritor.py
+++ db/escritor.py
@@ -1,5 +1,4 @@
 import os.path
-import re
 
 PATH = 'C:\\Users\\luisg\\OneDrive\\Ambiente de Trabalho\\PasswordGenerator\\db\\passwords.txt'
 
@@ -18,53 +17,54 @@ def filefunc(site, passe, escrever=False, ler=True):
     
     # Criação do Arquivo se não estiver criado
     try:
-        password_file = open(PATH, 'r+')
+        passwords_file = open(PATH, 'r+')
     except FileNotFoundError:
-        password_file = open(PATH, 'w+')
+        passwords_file = open(PATH, 'w+')
 
     # Leitura de linhas
-    leitura_file = password_file.read()
-    linhas = leitura_file.split('\n')
+    # leitura_file = passwords_file.read()
+    # linhas = leitura_file.split('\n')
     
-    for i in linhas:
-        if i:
-            n_linhas += 1    
+    # for i in linhas:
+    #     if i:
+    #         n_linhas += 1    
 
     if ler:
-        lista_site_pass = list()
-        lista_site_pass2 = list()
-        # Lê o arquiv 
+        lista_def = list()
+        lista_total = list()
+        
+        # Lê o arquivo
         while flag_leitura:
-
-            for i in password_file.readline():
-                lista_site_pass.append(i)
             
-            for item in range(0, len(lista_site_pass)):
-                string_site_pass += lista_site_pass[item]
+            # Lê as linhas
+            for k in passwords_file.readline():
+                string_site_pass += k
             
-            lista_site_pass.clear()
+            # TESTAR ANTES DE USAR -- POSSIVELMENTE NÃO NECESSÁRIO
+            # 
+            # Passa de Lista de carácteres para uma string
+            # for item in range(0, len(lista_def)):
+            #     string_site_pass += lista_def[item]
+            #     print(string_site_pass)
+                
+            # lista_def.clear()
             
-            lista_site_pass.append(string_site_pass)
+            lista_def.append(string_site_pass[:])
             
             # Remove site:  //   password:  //   \n
-            lista_site_pass = [frase.replace('site:', '') for frase in lista_site_pass]
-            lista_site_pass = [frase.replace('password: ', '') for frase in lista_site_pass]
-            lista_site_pass = [frase.replace('\n', '') for frase in lista_site_pass]
+            lista_def = [frase.replace('site:', '') for frase in lista_def]
+            lista_def = [frase.replace('password: ', '') for frase in lista_def]
+            lista_def = [frase.replace('\n', '') for frase in lista_def]
+
+            lista_total.append(lista_def[::])
+            lista_def.clear()
+            contador += 1
             
-            lista_site_pass2.append(lista_site_pass[:])
-            
-            # Remove todos os itens para depois ler a próxima linha
-            lista_site_pass.clear()
-            
-            if contador == n_linhas:
+            if contador == 3:
                 flag_leitura = False
-            else:
-                contador += 1
-        
-            
-        return lista_site_pass2
-            
-        
+                
+        return lista_total
+
             
             
-print(filefunc('sth', 'saidsainwlewla', escrever=False, ler=True))
+print(filefunc('Paypal', '0saAmGsainwl21ewla', escrever=True, ler=True))
