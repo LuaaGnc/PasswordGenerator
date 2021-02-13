1fe2d0d667510d8a3bf0b2c6f20831869886eea3
Luis-FutEng <luisgvs0302@gmail.com>
2021-02-13 11:42:54 +0000

    Criar funcionalidade de escrever no arquivo

--- db/escritor.py
+++ db/escritor.py
@@ -2,33 +2,52 @@ import os.path
 
 PATH = 'C:\\Users\\luisg\\OneDrive\\Ambiente de Trabalho\\PasswordGenerator\\db\\passwords.txt'
 
-def filefunc(site, passe, escrever=False, ler=True):
-    """Funcionalidades do arquivo FileFunc
+def filefunc(escrever=False, ler=False):
 
-    Args:
-        *args(string) -> Isto é o que será escrito: Site   --   Password
-        escrever (bool, optional): Se irá ser escrito ou não. Defaults to False.
-        ler (bool, optional): Se irá ser lido ou não. Defaults to True.
+    """Funcionalidades gerais para o arquivo passwords.txt
+
+    Returns:
+        Leitura dos Sites e Passwords OR Se foi escrito com sucesso no arquivo
     """
-   
     # Criação do Arquivo se não estiver criado
     try:
         passwords_file = open(PATH, 'r+')
+        passwords_file.close()
     except FileNotFoundError:
         passwords_file = open(PATH, 'w+')
+        passwords_file.close()
 
     # Lê o arquivo
-    if ler:
+    if ler: 
+        passwords_file = open(PATH, 'r')    
         lista_def = passwords_file.readlines()
 
-    # Remove site:  //   password:  //   \n
-    lista_def = [frase.replace('site:', '') for frase in lista_def]
-    lista_def = [frase.replace('password: ', '') for frase in lista_def]
-    lista_def = [frase.replace('\n', '') for frase in lista_def]
+        # Remove site:  //   password:  //   \n
+        lista_def = [frase.replace('site: ', '') for frase in lista_def]
+        lista_def = [frase.replace('password: ', '') for frase in lista_def]
+        lista_def = [frase.replace('\n', '') for frase in lista_def]
 
-    # lista_total recebe frase para cada frase contida em lista_def
-    lista_total = [[frase] for frase in lista_def]
-    return lista_total
+        # lista_total recebe frase para cada frase contida em lista_def
+        lista_total = [[frase] for frase in lista_def]
+        passwords_file.close()
+        
+        for i in lista_total:
+            print(i)
+    # Escreve no arquivo
+    if escrever:
+        # Abrir o arquivo no modo de "acrescento"
+        passwords_file = open(PATH, 'a+')
+        
+        site = str(input('Digite o -> nome do site <- para cadastrar sua password: ')).strip()
+        password = str(input('Digite a sua -> password <- : ')).strip()
+        
+        # Tenta escrever, Senão imprime um erro
+        try:
+            passwords_file.write(f'site: {site} password: {password}\n')
+            passwords_file.close()
+            print('CADASTRADO COM SUCESSO!')
+        except:
+            print('OCORREU UM ERRO NO SEU CADASTRO, TENTE NOVAMENTE!')
+        
 
-            
-print(filefunc('Paypal', '0saAmGsainwl21ewla', escrever=True, ler=True))
+print(filefunc(escrever=True, ler=True))
